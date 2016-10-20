295b313d788f038b9e4376b25e734bfa2fedc497
cxtan <chenxi.tan@ingenic.com>
2016-10-20 12:10:25 +0800

    add server and client

--- client.py
+++ client.py
@@ -1,1 +1,56 @@
+#!/bin/python
 __author__ = 'cxtan'
+import socket
+import sys, os
+import time
+
+def TCP_connect():
+    name = raw_input("input name:")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("192.168.4.16", 9999))
+    sock.send(name)
+    data = sock.recv(1024)
+    print data
+    name = name + ":"
+    while True:
+        in_data = raw_input(name)
+        sock.send(in_data)
+        if in_data == "exit":
+            break
+    sock.close()
+if __name__ == '__main__':
+    TCP_connect()
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.py
+++ server.py
@@ -1,1 +1,69 @@
+#!/bin/python
 __author__ = 'cxtan'
+
+import socket
+import os, sys
+import threading
+import time
+def new_clint(client, addr):
+    name = client.recv(1024)
+    print "A new client connect server addr: %s:%s" % addr,
+    print "client name %s:" % name
+    client.send("welcome cxtan server %s" % name)
+    while True:
+        data = client.recv(1024)
+        if data == 'exit' or not data:
+            break
+        print "%s:%s----->" % addr,
+        print "%s: %s" % (name, data)
+    client.close()
+    pass
+
+def server_end():
+    sock_ser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_ser.bind(("192.168.4.16", 9999))
+    sock_ser.listen(10)
+    print "wait client connect......."
+    while True:
+        client, addr = sock_ser.accept()
+        t = threading.Thread(target = new_clint, args = (client, addr))
+        t.start()
+
+if __name__ == '__main__':
+    server_end()
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
